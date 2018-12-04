831dbef3c6618163dd8a09b7611b8e32c47f3a77
Pascal Loth <pascal.loth@tuhh.de>
2018-12-04 09:36:10 +0100

    Fix installation for Ubuntu 18.04.1 LTS

--- termite-install.sh
+++ termite-install.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env sh
 
-sudo apt-get install -y \
-	git \
-	g++ \
-	libgtk-3-dev \
-	gtk-doc-tools \
-	gnutls-bin \
-	valac \
-	intltool \
-	libpcre2-dev \
-	libglib3.0-cil-dev \
-	libgnutls28-dev \
-	libgirepository1.0-dev \
-	libxml2-utils \
-	gperf
-	
+sudo apt-get install -y --allow-change-held-packages \
+  git \
+  g++ \
+  libgtk-3-dev \
+  gtk-doc-tools \
+  gnutls-bin \
+  valac \
+  intltool \
+  libpcre2-dev \
+  libglib3.0-cil-dev \
+  libgnutls28-dev \
+  libgirepository1.0-dev \
+  libxml2-utils \
+  gperf \
+  libgirepository1.0-dev
+
 git clone --recursive https://github.com/thestinger/termite.git
 git clone https://github.com/thestinger/vte-ng.git
 
